5242387b3e827f44366da414eb5abdac36cecc96
yefco <yigit.cokcetin@sabanciuniv.edu>
2026-03-19 22:39:33 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main()
+{
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("running on 0.0.0.0:3000");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+
+async fn root() -> &'static str
+{
+    "something something"
+
+}
